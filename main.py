0eadd9cccf94ae93103e41ab3b08256047ecb512
zpskek <wjdghdwns0@gmail.com>
2020-11-25 20:04:35 +0900

    Country time series

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import pandas as pd
 
+conditions = ["confirmed", "deaths", "recovered"]
+
 
 def make_global_df(condition):
     df = pd.read_csv(f"data/time_{condition}.csv")
     df = (
         df.drop(["Province/State", "Country/Region", "Lat", "Long"], axis=1)
+        .sum()
+        .reset_index(name=condition)
+    )
+    df = df.rename(columns={"index": "date"})
+    return df
+
+
+def make_country_df(condition, country):
+    df = pd.read_csv(f"data/time_{condition}.csv")
+    df = df.loc[df["Country/Region"] == country]
+    df = (
+        df.drop(columns=["Province/State", "Country/Region", "Lat", "Long"])
         .sum()
         .reset_index(name=condition)
     )
@@ -22,7 +36,7 @@ totals_df = totals_df.rename(columns={"index": "condition"})
 countries_df = daily_df[["Country_Region", "Confirmed", "Deaths", "Recovered"]]
 countries_df = countries_df.groupby("Country_Region").sum().reset_index()
 
-conditions = ["confirmed", "deaths", "recovered"]
+
 final_df = None
 
 for condition in conditions:
